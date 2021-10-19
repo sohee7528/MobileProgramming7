620feb7160597be69c4bb032482b14f72a7d90b1
sohee7528 <sohee7528@hanmail.net>
2021-10-20 03:18:27 +0900

    버튼- 전화, 홈페이지 열기, 웹서칭, 구글맵, 문자, 사진,

--- app/src/main/java/com/android/mobileprogramming7/MainActivity.java
+++ app/src/main/java/com/android/mobileprogramming7/MainActivity.java
@@ -3,8 +3,11 @@ package com.android.mobileprogramming7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewDebug;
 import android.widget.Button;
@@ -42,6 +45,68 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button btnDial=findViewById(R.id.btnDial);
+        Button btnWeb=findViewById(R.id.btnWeb);
+        Button btnGoogle=findViewById(R.id.btnGoogle);
+        Button btnSearch=findViewById(R.id.btnSearch);
+        Button btnSms=findViewById(R.id.btnSms);
+        Button btnPhoto=findViewById(R.id.btnPhoto);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri= Uri.parse("tel:010-1234-5678");
+                Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri= Uri.parse("http://www.swu.ac.kr");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri= Uri.parse("http://maps.google.com/maps?q="+37.62827+","+127.09048);
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY,"안드로이드");
+                startActivity(intent);
+            }
+        });
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body","안녕하세요?");
+                intent.setData(Uri.parse("smsto"+Uri.encode("010-1234-5678")));
+                startActivity(intent);
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
             }
         });
